aa640d0ace95bce1a9789e8ded5d4a48ceeeeccd
Jensen-3000 <118268364+Jensen-3000@users.noreply.github.com>
2025-08-05 14:06:53 +0200

    wip: layouts and todo lists

--- app/src/main/java/com/example/todolist/CreateActivity.java
+++ app/src/main/java/com/example/todolist/CreateActivity.java
@@ -1,6 +1,8 @@
 package com.example.todolist;
 
 import android.os.Bundle;
+import android.widget.NumberPicker;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class CreateActivity extends AppCompatActivity {
+
+    NumberPicker pickPoints;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,20 @@ public class CreateActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        initGui();
+    }
+
+    private void initGui() {
+        pickPoints = findViewById(R.id.num_points);
+        pickPoints.setMinValue(1);
+        pickPoints.setMaxValue(10);
+        pickPoints.setValue(5);
+        pickPoints.setWrapSelectorWheel(false);
+
+        pickPoints.setOnValueChangedListener((pickPoints, oldVal, newVal) -> {
+            Toast.makeText(this, "Points selected: " + newVal, Toast.LENGTH_SHORT).show();
         });
     }
 }
